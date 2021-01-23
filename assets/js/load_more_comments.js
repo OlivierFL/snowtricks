e072d4ede8b99f485d92ff435777adceb41cc4b8
Olivier Floch <o.floch@protonmail.com>
2021-01-23 17:46:39 +0100

    Rework Ajax request for trick detail page

--- assets/js/load_more_comments.js
+++ assets/js/load_more_comments.js
@@ -3,44 +3,77 @@ import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/pu
 
 Routing.setRoutingData(routes);
 
-$(document).ready(function () {
-    const limit = $('.comment').length;
-    let offset = limit;
-    let spinner = $('#spinner');
+const limit = $('.comment').length;
+let offset = limit;
+let spinner = $('#spinner-comments');
 
-    $("#load-more").click(function () {
-        spinner.removeClass('hidden');
-        spinner.addClass('animate-spin');
-        $.getJSON(
-            Routing.generate('load_more_comments', {
-                offset: offset,
-                limit: limit
-            }), function (results) {
-                console.log(results);
-                if (0 === results.length) {
-                    $('#load-more').html('Plus aucun commentaire').removeClass('bg-yellow-500 hover:bg-yellow-600').addClass('cursor-not-allowed text-black bg-gray-100');
-                    return;
-                }
-                display(results);
-            }).done(function () {
-            spinner.addClass('hidden');
-            spinner.removeClass('animate-spin');
-            offset += limit;
-        });
+$("#load-more-comments-btn").click(function () {
+    spinner.removeClass('hidden');
+    spinner.addClass('animate-spin');
+    $.getJSON(
+        Routing.generate('load_more_comments', {
+            offset: offset,
+            limit: limit
+        }), function (results) {
+            if (0 === results.length) {
+                $('#load-more-comments-btn').html('Plus aucun commentaire').removeClass('bg-yellow-500 hover:bg-yellow-600').addClass('cursor-not-allowed text-black bg-gray-100');
+                return;
+            }
+            display(results);
+        }).done(function () {
+        spinner.addClass('hidden');
+        spinner.removeClass('animate-spin');
+        offset += limit;
     });
+});
 
-    function display(results) {
-        $.each(results, function (key, result) {
+function display(results) {
+    $.each(results, function (key, result) {
+        let updatedAt = formatUpdatedAt();
+        if (!result.author.avatar) {
             $('#comments-list').append(
                 `<div class="flex flex-row justify-center mb-10 comment">
-                    <div class="w-2/12 flex flex-col justify-center items-center text-center text-gray-500 mr-2 pr-4">
-                      <i class="fas fa-user border border-gray-500 rounded-full p-2"></i>
-                      <p class="font-light text-sm">${result.author.username}</p>
+                        <div class="w-2/12 flex flex-col justify-center items-center text-center text-gray-500 mr-2 pr-4">
+                           <div class="flex justify-center items-center w-6 h-6 lg:w-8 lg:h-8 rounded-full border-2 border-yellow-500 bg-gray-500 text-white p-5 capitalize"
+                         title="${result.author.username}">
+                             <div class="text-xl cursor-default">${result.author.username.slice(0, 1)}</div>
+                           </div>
+                          <p class="font-light text-sm mb-2">${result.author.username}</p>
+                          <p class="font-light text-sm">${updatedAt}</p>
+                        </div>
+                        <div class="w-10/12 p-2 border-b-2 border-l-2 border-yellow-500 text-sm ml-auto">
+                          <p>${result.content}</p>
+                      </div>
+                  </div>
+                `);
+        } else {
+            $('#comments-list').append(
+                `<div class="flex flex-row justify-center mb-10 comment">
+                        <div class="w-2/12 flex flex-col justify-center items-center text-center text-gray-500 mr-2 pr-4">
+                            <img src="/uploads/avatars/${result.author.avatar}"
+                                 alt="${result.author.username}"
+                                 title="${result.author.username}"
+                                 class="object-cover w-11 h-11 rounded-full border-yellow-500 border-2"
+                                 width="44"
+                                 height="44"
+                            >
+                      <p class="font-light text-sm mb-2">${result.author.username}</p>
+                      <p class="font-light text-sm">${updatedAt}</p>
                     </div>
                     <div class="w-10/12 p-2 border-b-2 border-l-2 border-yellow-500 text-sm ml-auto">
                       <p>${result.content}</p>
                     </div>
-                </div>`);
-        });
-    }
-});
+                </div>
+                `)
+        }
+
+        function formatUpdatedAt() {
+            let day = new Intl.DateTimeFormat('fr-FR').format(new Date(result.updatedAt));
+            let hour = new Intl.DateTimeFormat('fr-FR', {
+                hour: '2-digit',
+                minute: '2-digit'
+            }).format(new Date(result.updatedAt))
+            return day + ' à ' + hour;
+        }
+    });
+}
--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -13,25 +13,38 @@ use Symfony\Component\Serializer\SerializerInterface;
 class AjaxController extends AbstractController
 {
     /**
+     * @var SerializerInterface
+     */
+    private SerializerInterface $serializer;
+
+    /**
+     * AjaxController constructor.
+     *
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
      * @Route("/tricks/load-more/{offset}/{limit}",
      *     name="load_more_tricks",
      *     options={"expose"=true},
      *     requirements={"offset"="\d+", "limit"="\d+"}
      * )
-     * @param TrickRepository     $repository
-     * @param SerializerInterface $serializer
-     * @param int                 $offset
-     * @param int                 $limit
+     * @param TrickRepository $repository
+     * @param int             $offset
+     * @param int             $limit
      *
      * @return JsonResponse
      */
     public function loadMoreTricks(
         TrickRepository $repository,
-        SerializerInterface $serializer,
         int $offset = 0,
         int $limit = 4
     ): JsonResponse {
-        return $this->loadMoreResults($repository, $limit, $offset, $serializer);
+        return $this->loadMoreResults($repository, $limit, $offset, 'createdAt');
     }
 
     /**
@@ -40,35 +53,37 @@ class AjaxController extends AbstractController
      *     options={"expose"=true},
      *     requirements={"offset"="\d+", "limit"="\d+"}
      * )
-     * @param CommentRepository   $repository
-     * @param SerializerInterface $serializer
-     * @param int                 $offset
-     * @param int                 $limit
+     * @param CommentRepository $repository
+     * @param int               $offset
+     * @param int               $limit
      *
      * @return JsonResponse
      */
     public function loadMoreComments(
         CommentRepository $repository,
-        SerializerInterface $serializer,
         int $offset = 0,
         int $limit = 4
     ): JsonResponse {
-        return $this->loadMoreResults($repository, $limit, $offset, $serializer);
+        return $this->loadMoreResults($repository, $limit, $offset, 'updatedAt');
     }
 
     /**
      * @param ServiceEntityRepositoryInterface $repository
      * @param int                              $limit
      * @param int                              $offset
-     * @param SerializerInterface              $serializer
+     * @param string                           $param
      *
      * @return JsonResponse
      */
-    private function loadMoreResults(ServiceEntityRepositoryInterface $repository, int $limit, int $offset, SerializerInterface $serializer): JsonResponse
-    {
-        $query = $repository->findBy([], ['createdAt' => 'DESC'], $limit, $offset);
+    private function loadMoreResults(
+        ServiceEntityRepositoryInterface $repository,
+        int $limit,
+        int $offset,
+        string $param
+    ): JsonResponse {
+        $query = $repository->findBy([], [$param => 'DESC'], $limit, $offset);
 
-        $data = $serializer->serialize($query, 'json', [
+        $data = $this->serializer->serialize($query, 'json', [
             'circular_reference_handler' => function ($object) {
                 return $object->getId();
             },
