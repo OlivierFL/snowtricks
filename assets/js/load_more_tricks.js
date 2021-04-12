2edef8c533a1e754a701652045d7117e99902283
Olivier Floch <o.floch@protonmail.com>
2021-04-12 20:58:21 +0200

    refactor: load more tricks javascript

--- assets/js/load_more_tricks.js
+++ assets/js/load_more_tricks.js
@@ -2,17 +2,15 @@ import Routing from './helper/routing';
 
 export function displayTricks(results) {
     $.each(results, function (key, result) {
-        let coverImageUrl = null;
-        let coverImageAltText = null;
-        for (const trickMedia of result.tricksMedia) {
-            if (true === trickMedia.isCoverImage) {
-                coverImageUrl = trickMedia.media.url;
-                coverImageAltText = trickMedia.media.altText;
-            }
-        }
-        $('#tricks-list').append(
-            `<div class="flex flex-col bg-gray-100 rounded-md shadow-lg overflow-hidden trick">
-              <a href="${Routing.generate('trick_detail', {slug: result.slug})}">
+        let {coverImageUrl, coverImageAltText} = getCoverImage(result);
+        addHtmlTemplate(result, coverImageUrl, coverImageAltText);
+    });
+}
+
+function addHtmlTemplate(trick, coverImageUrl, coverImageAltText) {
+    $('#tricks-list').append(
+        `<div class="flex flex-col bg-gray-100 rounded-md shadow-lg overflow-hidden trick">
+              <a href="${Routing.generate('trick_detail', {slug: trick.slug})}">
                   <div class="container">
                       <img src="${coverImageUrl ? "/uploads/tricks/" + coverImageUrl : "/build/img/hero.jpg"}"
                          alt="${coverImageAltText ?? "Snowtricks hero image"}"
@@ -24,14 +22,25 @@ export function displayTricks(results) {
                   </div>
               </a>
                   <div class="flex flex-row mt-auto justify-between items-center p-4">
-                    <a href="${Routing.generate('trick_detail', {slug: result.slug})}" class="p-2 font-bold">${result.name}</a>
+                    <a href="${Routing.generate('trick_detail', {slug: trick.slug})}" class="p-2 font-bold">${trick.name}</a>
                     <div class="ml-auto">
-                        <a href="${Routing.generate('trick_edit', {slug: result.slug})}">
+                        <a href="${Routing.generate('trick_edit', {slug: trick.slug})}">
                             <i class="cursor-pointer fas fa-pen mr-2 text-blue-700 hover:text-blue-800"></i>
                         </a>
                         <i class="fas fa-trash-alt text-red-600 hover:text-red-700"></i>
                     </div>
               </div>
             </div>`);
-    });
+}
+
+function getCoverImage(trick) {
+    let coverImageUrl = null;
+    let coverImageAltText = null;
+    for (const trickMedia of trick.tricksMedia) {
+        if (true === trickMedia.isCoverImage) {
+            coverImageUrl = trickMedia.media.url;
+            coverImageAltText = trickMedia.media.altText;
+        }
+    }
+    return {coverImageUrl, coverImageAltText};
 }
